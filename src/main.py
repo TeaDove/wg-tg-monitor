92093878a6a90a049efd31edb72f066e3bf8fc1a
TeaDove <peter.ibragimov@gmail.com>
2021-03-10 16:13:15 +0100

    hotfix

--- src/main.py
+++ src/main.py
@@ -31,25 +31,26 @@ def get_peers_dict() -> dict:
 
 
 @dp.message_handler(commands = "wg")
-async def send(message: types.Message): 
-    wg_output = subprocess.run(["wg"], capture_output=True).stdout.decode()
-    peers = wg_output.split("peer: ")
-    #logging.warn(peers)
-    lines = peers[0].strip("\n").splitlines() 
-    str_to_send = ''
-    for line in lines:
-        parts = line.split(':', 1)
-        str_to_send += "    <b>" + parts[0].strip() + ": </b>" + parts[1].strip() + "\n"
-    await message.reply(str_to_send, parse_mode="html")
-    for peer in peers[1:]:
-        lines = peer.strip("\n").splitlines()
-        str_to_send = "peer: " + lines[0] + "\n"
-        for line in lines[1:]:
+async def send(message: types.Message):
+    if message.from_user.id == int(config["credentials"]["owner_id"]):
+        wg_output = subprocess.run(["wg"], capture_output=True).stdout.decode()
+        peers = wg_output.split("peer: ")
+        #logging.warn(peers)
+        lines = peers[0].strip("\n").splitlines() 
+        str_to_send = ''
+        for line in lines:
             parts = line.split(':', 1)
             str_to_send += "    <b>" + parts[0].strip() + ": </b>" + parts[1].strip() + "\n"
-        if lines[0] in PEERS_DICT:
-            str_to_send += "    <b>name:</b><i> " + PEERS_DICT[lines[0]] + "</i>\n"
-        await message.answer(str_to_send, parse_mode="html")
+        await message.reply(str_to_send, parse_mode="html")
+        for peer in peers[1:]:
+            lines = peer.strip("\n").splitlines()
+            str_to_send = "peer: " + lines[0] + "\n"
+            for line in lines[1:]:
+                parts = line.split(':', 1)
+                str_to_send += "    <b>" + parts[0].strip() + ": </b>" + parts[1].strip() + "\n"
+            if lines[0] in PEERS_DICT:
+                str_to_send += "    <b>name:</b><i> " + PEERS_DICT[lines[0]] + "</i>\n"
+            await message.answer(str_to_send, parse_mode="html")
 
 if __name__ == '__main__':
     get_peers_dict()
